75907047e6e66fe9f8b6e510fd13c5cfd155a2ff
Frade11 <gurdisartemii@gmail.com>
2025-10-13 00:44:34 +0300

    js added

--- student_edit_load.js
+++ student_edit_load.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
-})
+    fetch('redirect_to_edit.php').then(Response => Response.json())
+    .then(student =>{
+        if(student && student.IDELEV){
+            document.getElementById('editName').value = student.NUMELE || '';
+            document.getElementById('editLName').value = student.PRENUMELE || '';
+            document.getElementById('editAdress').value = student.Adresa || '';
+            document.getElementById('editEmail').value = student.Email || '';
+            document.getElementById('editDate').value = student.DataNasterii || '';
+            document.getElementById('editSex').value = student.SEX || '';
+            document.getElementById('editAvg').value = student.NotaMedieBac || '';
+        }
+    })
+    .catch(error =>{
+        console.error('Eroare:', error);
+    });
+})  
